9c63f56edf7054b5a712c195e9ca60f5c459ddb1
Vivian <vynride@gmail.com>
2025-09-30 23:19:56 +0530

    feat: migrate to fumadocs upto vectorization

---
+++ source.config.ts
@@ -0,0 +1,31 @@
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+
+import {
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+  metaSchema,
+} from 'fumadocs-mdx/config';
+
+// You can customise Zod schemas for frontmatter and `meta.json` here
+// see https://fumadocs.dev/docs/mdx/collections
+export const docs = defineDocs({
+  docs: {
+    schema: frontmatterSchema,
+    postprocess: {
+      includeProcessedMarkdown: true,
+    },
+  },
+  meta: {
+    schema: metaSchema,
+  },
+});
+
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    // Place it at first, it should be executed before the syntax highlighter
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+  },
+});
